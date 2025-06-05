794b36ae1f65c03dc77d199097c1d9ec86b94124
Partho Protim Roy <parthoroy158@gmail.com>
2025-06-06 00:52:20 +0600

    jwt finished

--- src/Hooks/UseAxiosPrivate.jsx
+++ src/Hooks/UseAxiosPrivate.jsx
@@ -3,6 +3,16 @@ const AxiosPrivate = axios.create({
     baseURL: 'http://localhost:5000'
 })
 const UseAxiosPrivate = () => {
+    AxiosPrivate.interceptors.request.use(function (config) {
+        const token = localStorage.getItem("Access-Token")
+        console.log("this is the token:", token)
+        config.headers.Authorization = `bearer ${token}`
+        // Do something before request is sent
+        return config;
+    }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+    });
     return AxiosPrivate
 };
 
